3c61a0f0bd2d480570814a8d230d580bc7aacc0a
Thibaut <thibaut.guibert@edu.escp.eu>
2020-11-19 18:47:03 +0100

    added signs icons in resultats

--- src/Components/Resultats.jsx
+++ src/Components/Resultats.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import './Resultats.css';
 import Troubadour from './Troubadour'
+import arrowLeft from '../Images/arrow-left.svg';
+import arrowRight from '../Images/arrow-right.svg';
+import arrowTop from '../Images/arrow-top.svg';
+import arrowTopLeft from '../Images/arrow-top-left.svg';
+import arrowTopRight from '../Images/arrow-top-right.svg';
+
 
 const signs = {
     "- 98": "↶",
     "-8": "↶",
-    "-7": "↖",
-    "-3": "↰",
-    "-2": "↰",
-    "-1": "↖",
-    "0": "↑",
-    "1": "↗",
-    "2": "→",
-    "3": "→",
-    "4": "↑",
-    "5": "↑",
+    "-7": arrowTopLeft,
+    "-3": arrowLeft,
+    "-2": arrowLeft,
+    "-1": arrowTopLeft,
+    "0": arrowTop,
+    "1": arrowTopRight,
+    "2": arrowRight,
+    "3": arrowRight,
+    "4": arrowTop,
+    "5": arrowTop,
     "6": "↻",
-    "7": "↗",
+    "7": arrowTopRight,
     "8": "↺"
 };
+
+// (if signs === "0") {
+//  phrase = "${text} sur ${distance > 1000 ? distance.arrondir au km : distance.arrondir au centaine de metre}"
+// } else phrase = "Dans ${distance > 1000 ? distance.arrondir au km : distance.arrondir au centaine de metre}, ${text}";
 
 function Resultats() {
     return (
