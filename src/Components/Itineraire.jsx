aebc3a1351ca0ff131dd3f01628eaceffe926f5e
Sarah Q <sarahquero@gmail.com>
2020-11-20 00:57:59 +0100

    finalized api champ

--- src/Components/Itineraire.jsx
+++ src/Components/Itineraire.jsx
@@ -1,13 +1,169 @@
 import React from 'react';
-import SearchAddress from './SearchAddress';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import './Itineraire.css';
 
-function Itineraire() {
-  return (
-    <div>
-      <SearchAddress />
-    </div>
-  );
+class Itineraire extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiKey: 'c538faf0-14b9-4af5-8901-07cbc87d8b26',
+      departure: '',
+      arrival: '',
+      resultApiDep: [],
+      resultApiArr: [],
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { departure, arrival } = this.state;
+    if (prevState.departure !== departure && departure.length > 3) {
+      this.callApiDep();
+    }
+    if (prevState.arrival !== arrival && arrival.length > 3) {
+      this.callApiArr();
+    }
+  }
+
+  handleChangeDeparture = (event) => {
+    this.setState({ departure: event.target.value });
+  };
+
+  handleChangeArrival = (event) => {
+    this.setState({ arrival: event.target.value });
+  };
+
+  handleClickDep = (event) => {
+    const { resultApiDep } = this.state;
+    const { depPoint } = this.props;
+    const id = parseInt(event.target.id, 10);
+    const selectLat = resultApiDep[id].point.lat;
+    const selectLong = resultApiDep[id].point.lng;
+    console.log(depPoint);
+    depPoint.latitude = selectLat;
+    depPoint.longitude = selectLong;
+    this.setState({
+      resultApiDep: [],
+    });
+  };
+
+  handleClickArr = (event) => {
+    const { resultApiArr } = this.state;
+    const { arrPoint } = this.props;
+    const id = parseInt(event.target.id, 10);
+    const selectLat = resultApiArr[id].point.lat;
+    const selectLong = resultApiArr[id].point.lng;
+    console.log(arrPoint);
+    arrPoint.latitude = selectLat;
+    arrPoint.longitude = selectLong;
+    this.setState({
+      resultApiArr: [],
+    });
+  };
+
+  callApiArr() {
+    const { arrival, apiKey } = this.state;
+    axios
+      .get(
+        `https://graphhopper.com/api/1/geocode?q=${arrival}&locale=fr&debug=true&key=${apiKey}`
+      )
+      .then((res) => this.setState({ resultApiArr: res.data.hits }));
+  }
+
+  callApiDep() {
+    const { departure, apiKey } = this.state;
+    axios
+      .get(
+        `https://graphhopper.com/api/1/geocode?q=${departure}&locale=fr&debug=true&key=${apiKey}`
+      )
+      .then((res) => this.setState({ resultApiDep: res.data.hits }));
+  }
+
+  render() {
+    const { departure, arrival, resultApiDep, resultApiArr } = this.state;
+    const {
+      handleChangeArrival,
+      handleChangeDeparture,
+      handleClickDep,
+      handleClickArr,
+    } = this;
+    return (
+      <div>
+        <form className="itineraryAddress">
+          <input
+            type="text"
+            id="departure"
+            name="departure"
+            value={departure}
+            placeholder="Départ"
+            onChange={handleChangeDeparture}
+          />
+          <input
+            type="text"
+            id="arrival"
+            name="arrival"
+            value={arrival}
+            placeholder="Arrivée"
+            onChange={handleChangeArrival}
+          />
+        </form>
+        <ul>
+          {resultApiDep.map((city, index) => (
+            <li key={city.osm_id}>
+              <button type="button" id={index} onClick={handleClickDep}>
+                {city.name} {city.state}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {resultApiArr.map((city, index) => (
+            <li key={city.osm_id}>
+              <button type="button" id={index} onClick={handleClickArr}>
+                {city.name} {city.state}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button type="button">
+          <Link to="/Resultats">Result</Link>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Itineraire;
+
+// import React from 'react';
+// import SearchAddress from './SearchAddress';
+// import './Itineraire.css';
+
+// function Itineraire(props) {
+//   const {
+//     departure,
+//     arrival,
+//     resultApiDep,
+//     resultApiArr,
+//     handleChangeArrival,
+//     handleChangeDeparture,
+//     handleClickDep,
+//     handleClickArr,
+//   } = props;
+//   return (
+//     <div>
+//       <SearchAddress
+//         departure={departure}
+//         arrival={arrival}
+//         resultApiDep={resultApiDep}
+//         resultApiArr={resultApiArr}
+//         handleChangeArrival={handleChangeArrival}
+//         handleChangeDeparture={handleChangeDeparture}
+//         handleClickDep={handleClickDep}
+//         handleClickArr={handleClickArr}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Itineraire;
