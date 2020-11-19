5657e78c0df7970140c37084165715251a32204f
Thibaut <thibaut.guibert@edu.escp.eu>
2020-11-19 17:35:34 +0100

    added signs meaning

--- src/Components/Resultats.jsx
+++ src/Components/Resultats.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import './Resultats.css';
 import Troubadour from './Troubadour'
 
-function Resultats(){
-    return(
+const signs = {
+    "- 98": "↶",
+    "-8": "↶",
+    "-7": "↖",
+    "-3": "↰",
+    "-2": "↰",
+    "-1": "↖",
+    "0": "↑",
+    "1": "↗",
+    "2": "→",
+    "3": "→",
+    "4": "↑",
+    "5": "↑",
+    "6": "↻",
+    "7": "↗",
+    "8": "↺"
+};
+
+function Resultats() {
+    return (
         <div>
             <Troubadour />
         </div>
