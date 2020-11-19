73a9ca85af73942afb85fb61ed6a32437ae68ca7
Sovakiiir <valentin.broudinpro@gmail.com>
2020-11-19 16:28:26 +0100

    maj

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,8 +3,13 @@ import './Home.css';
 
 function Home(){
     return(
-        <div>
-            <p>test</p>
+        <div className="home">
+            <div className="title">La Mappe-Monde Magique</div>
+            <div className="texte1">Celle qui te dit ou que c'est que tu dois aller pour sauver Cunégonde</div>
+            <div className="Button">
+                <button className="texteButton">Choisir ma monture !</button>
+            </div>
+            
         </div>
     )
 }
