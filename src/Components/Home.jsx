5840c48cb3ed34dd819468f6d7e3fb1fd2bf7987
Sovakiiir <valentin.broudinpro@gmail.com>
2020-11-20 09:29:54 +0100

    Maj responsive home et changement css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,17 +6,19 @@ import Troubadour from './Troubadour';
 function Home() {
   return (
     <div className="home">
-      <div className="title">La Mappe-Monde Magique</div>
-      <div className="texte1">
-        Celle qui te dit ou que c'est que tu dois aller pour sauver Cunégonde
-      </div>
-      <Link to="/Monture">
-        <div className="Button">
-          <button type="button" className="texteButton">
-            Choisir ma monture !
-          </button>
+      <div className="Philippe">
+        <div className="title">La Mappe-Monde Magique</div>
+        <div className="texte1">
+            Celle qui te dit ou que c'est que tu dois aller pour sauver Cunégonde
         </div>
-      </Link>
+        <Link to="/Monture">
+            <div className="Button">
+            <button type="button" className="texteButton">
+                Choisir ma monture !
+            </button>
+            </div>
+        </Link>
+      </div>
       <div>
         <Troubadour />
       </div>
