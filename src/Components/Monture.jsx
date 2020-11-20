8175ef3e72c56ee38f4f2d3b66ab5486a209390d
Sarah Q <sarahquero@gmail.com>
2020-11-20 13:21:37 +0100

    modified state for vehicle

--- src/Components/Monture.jsx
+++ src/Components/Monture.jsx
@@ -8,146 +8,125 @@ import Parchemin from '../Images/parchemin.png';
 import './Monture.css';
 import Troubadour from './Troubadour';
 
-class Monture extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      monture: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    const { monture } = this.state;
-    this.setState({ monture: event.target.id });
-    console.log(monture);
-    console.log(event.target.id);
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="monture">
-          <h1 className="titreMonture">L'ecurie magique</h1>
-          <div className="choixMonture">
-            <NavLink
-              className="dragonLink"
-              activeClassName="active"
-              to="/Itineraire"
-            >
-              <div className="dragon">
+function Monture({ handleClick }) {
+  return (
+    <div>
+      <div className="monture">
+        <h1 className="titreMonture">L'ecurie magique</h1>
+        <div className="choixMonture">
+          <NavLink
+            className="dragonLink"
+            activeClassName="active"
+            to="/Itineraire"
+          >
+            <div className="dragon">
+              <img
+                className="imageDragon"
+                src={Dragon}
+                id="car"
+                onClick={handleClick}
+                alt="imageDragon"
+              ></img>
+              <div className="parcheminTexteD">
                 <img
-                  className="imageDragon"
-                  src={Dragon}
+                  className="imageParchemin"
+                  src={Parchemin}
                   id="car"
-                  onClick={this.handleClick}
-                  alt="imageDragon"
+                  onClick={handleClick}
+                  alt="imageParchemin"
                 ></img>
-                <div className="parcheminTexteD">
-                  <img
-                    className="imageParchemin"
-                    src={Parchemin}
-                    id="car"
-                    onClick={this.handleClick}
-                    alt="imageParchemin"
-                  ></img>
-                  <p className="enDragon" id="car" onClick={this.handleClick}>
-                    en dragon
-                  </p>
-                </div>
+                <p className="enDragon" id="car" onClick={handleClick}>
+                  en dragon
+                </p>
               </div>
-            </NavLink>
-            <NavLink
-              className="piedLink"
-              activeClassName="active"
-              to="/Itineraire"
-            >
-              <div className="pied">
+            </div>
+          </NavLink>
+          <NavLink
+            className="piedLink"
+            activeClassName="active"
+            to="/Itineraire"
+          >
+            <div className="pied">
+              <img
+                className="imagePied"
+                src={Pied}
+                id="foot"
+                onClick={handleClick}
+                alt="imagePied"
+              ></img>
+              <div className="parcheminTexteP">
                 <img
-                  className="imagePied"
-                  src={Pied}
+                  className="imageParchemin"
+                  src={Parchemin}
                   id="foot"
-                  onClick={this.handleClick}
-                  alt="imagePied"
+                  onClick={handleClick}
+                  alt="imageParchemin"
                 ></img>
-                <div className="parcheminTexteP">
-                  <img
-                    className="imageParchemin"
-                    src={Parchemin}
-                    id="foot"
-                    onClick={this.handleClick}
-                    alt="imageParchemin"
-                  ></img>
-                  <p className="aPied" id="foot" onClick={this.handleClick}>
-                    a pied
-                  </p>
-                </div>
+                <p className="aPied" id="foot" onClick={handleClick}>
+                  a pied
+                </p>
               </div>
-            </NavLink>
-            <NavLink
-              className="fumantLink"
-              activeClassName="active"
-              to="/Itineraire"
-            >
-              <div className="fumant">
+            </div>
+          </NavLink>
+          <NavLink
+            className="fumantLink"
+            activeClassName="active"
+            to="/Itineraire"
+          >
+            <div className="fumant">
+              <img
+                className="imageFumant"
+                src={Fumant}
+                id="scooter"
+                onClick={handleClick}
+                alt="imageFumant"
+              ></img>
+              <div className="parcheminTexteF">
                 <img
-                  className="imageFumant"
-                  src={Fumant}
+                  className="imageParchemin"
+                  src={Parchemin}
                   id="scooter"
-                  onClick={this.handleClick}
-                  alt="imageFumant"
+                  onClick={handleClick}
+                  alt="imageParchemin"
                 ></img>
-                <div className="parcheminTexteF">
-                  <img
-                    className="imageParchemin"
-                    src={Parchemin}
-                    id="scooter"
-                    onClick={this.handleClick}
-                    alt="imageParchemin"
-                  ></img>
-                  <p
-                    className="enFumant"
-                    id="scooter"
-                    onClick={this.handleClick}
-                  >
-                    en fumant
-                  </p>
-                </div>
+                <p className="enFumant" id="scooter" onClick={handleClick}>
+                  en fumant
+                </p>
               </div>
-            </NavLink>
-            <NavLink
-              className="chevalLink"
-              activeClassName="active"
-              to="/Itineraire"
-            >
-              <div className="cheval">
+            </div>
+          </NavLink>
+          <NavLink
+            className="chevalLink"
+            activeClassName="active"
+            to="/Itineraire"
+          >
+            <div className="cheval">
+              <img
+                className="imageCheval"
+                src={Cheval}
+                id="bike"
+                onClick={handleClick}
+                alt="imageCheval"
+              ></img>
+              <div className="parcheminTexteC">
                 <img
-                  className="imageCheval"
-                  src={Cheval}
+                  className="imageParchemin"
+                  src={Parchemin}
                   id="bike"
-                  onClick={this.handleClick}
-                  alt="imageCheval"
+                  onClick={handleClick}
+                  alt="imageParchemin"
                 ></img>
-                <div className="parcheminTexteC">
-                  <img
-                    className="imageParchemin"
-                    src={Parchemin}
-                    id="bike"
-                    onClick={this.handleClick}
-                    alt="imageParchemin"
-                  ></img>
-                  <p className="aCheval" id="bike" onClick={this.handleClick}>
-                    a cheval
-                  </p>
-                </div>
+                <p className="aCheval" id="bike" onClick={handleClick}>
+                  a cheval
+                </p>
               </div>
-            </NavLink>
-          </div>
+            </div>
+          </NavLink>
         </div>
-        <Troubadour texte="Quelle belle bête avez-vous l'habitude de monter? Et je ne parle pas de Cunégonde" />
       </div>
-    );
-  }
+      <Troubadour texte="Quelle belle bête avez-vous l'habitude de monter? Et je ne parle pas de Cunégonde" />
+    </div>
+  );
 }
 
 export default Monture;
